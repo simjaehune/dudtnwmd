0d24b6f898520fb2db2fbde6a32bacf696cd561e
simjaehune <86578389+simjaehune@users.noreply.github.com>
2021-07-01 16:15:57 +0900

    Add files via upload

---
+++ src/OrderListArray.java
@@ -0,0 +1,20 @@
+package dus;
+
+public class OrderListArray {
+	
+	String name="";
+	int no=0;
+	int price=0;
+	int count=0;
+	int total=0;
+
+	public OrderListArray(String name, int no, int price, int count, int total)
+	{
+		this.name = name;
+		this.no = no;
+		this.price = price;
+		this.count = count;
+		this.total = total;
+	}
+	
+}
